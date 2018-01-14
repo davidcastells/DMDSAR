1406f50010bf2ea8dc4df86a849521612fb3b7f0
David Castells-Rufas <david.castells@uab.es>
2018-01-14 10:53:06 +0100

    initial

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=MSYS2-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MSYS2-Windows
+CND_ARTIFACT_NAME_Debug=DMDSAR
+CND_ARTIFACT_PATH_Debug=dist/Debug/MSYS2-Windows/DMDSAR
+CND_PACKAGE_DIR_Debug=dist/Debug/MSYS2-Windows/package
+CND_PACKAGE_NAME_Debug=DMDSAR.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MSYS2-Windows/package/DMDSAR.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=DMDSAR
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/DMDSAR
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=DMDSAR.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/DMDSAR.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
